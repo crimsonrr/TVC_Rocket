a5f538770a2e89715b30b699c1e4e3d33b7e7f31
Raayan Ahmed <ironspiderp8@gmail.com>
2026-06-14 01:09:56 -0400

    added test code for the gimbal mechanism

---
+++ test_scripts/gimbalcalibration.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+#include <Servo.h>
+
+Servo pitchServo;
+Servo yawServo;
+
+void setup() {
+    pitchServo.attach(9);
+    yawServo.attach(10);
+
+    pitchServo.writeMicroseconds(1500); 
+    yawServo.writeMicroseconds(1500);
+
+    delay(2000);
+
+}
+
+void loop() {
+// start slightly to the left, and increment to the right
+    for (int pos = 1350; pos < 1650; pos += 5) {
+        pitchServo.writeMicroseconds(pos);
+
+    delay(15);
+    }
+
+        for (int pos = 1650; pos >= 1350; pos -= 5) {
+            pitchServo.writeMicroseconds(pos);
+        
+            delay(15);
+        }
+
+    pitchServo.writeMicroseconds(1500);
+    delay(1000);
+}
